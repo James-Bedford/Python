974a7c0ff2bec8b3d5cf2bead60ceacbcb9b40e1
James-Bedford <jamesbedford01@hotmail.com>
2016-12-07 10:25:56 +0000

    Add files via upload

---
+++ for_the_record.py
@@ -0,0 +1,29 @@
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+students = [lloyd, alice, tyler]
+
+for student_info in students:
+    print (student_info["name"])
+    print (student_info["homework"])
+    print (student_info["quizzes"])
+    print (student_info["tests"])
+    #print (" "+student_info[student_info])
+    #need to use name in list as holder to dictionary to reference
+    #name, hoemwork, quizzes and tests
---
+++ hello.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from math import *
+print("Hello")
+now = datetime.now()
+print(" "+str(now.day)+" "+str(now.year))
+print (" "+str(now.month)+" "+str(now.day)+" "+str(now.year))
+
+print (" "+str(sqrt(25)))
