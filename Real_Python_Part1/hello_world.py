01e9e585abd7a9608daa3e4c47e9f38e701add9d
James Bedford <jamesbedford01@hotmail.com>
2017-01-10 15:34:36 +0000

    Folder for Real Python Course
    Part one exercise repository

---
+++ Real_Python_Part1/hello_world.py
@@ -0,0 +1,19 @@
+#First set of exercises from RPP1 p23
+greeting = "Hello World!"
+print (greeting)
+'''
+lots
+of comments
+'''
+print ("Ah ah")
+conversation = 'I said , "put it over by the llama"'
+print (conversation)
+
+number = "123"
+print (number)
+
+#to preserve white spaces
+
+print("""this is a string that spans across \
+multiple lines that also preserves whitespaces""")
+
