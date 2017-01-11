56bb1ced64fb19a8e85505761c09d05847b10d13
James Bedford <jamesbedford01@hotmail.com>
2017-01-11 12:45:13 +0000

    11 Jan

--- Real_Python_Part1/Hey_whats_up.py
+++ Real_Python_Part1/Hey_whats_up.py
@@ -1,3 +1,9 @@
 #printout and then waits for user input
 user_input = input("Hey what's up?")
 print("You said: ", user_input)
+
+
+response = input("What should I shout?")
+response = response.upper()
+print("Well, if you insist...", response)
+
