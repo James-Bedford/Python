8514e90703f436588a78e3f9f16ff02601a3f76c
James Bedford <jamesbedford01@hotmail.com>
2017-01-12 14:21:05 +0000

    find script added 12 1 17

---
+++ Real_Python_Part1/find_input.py
@@ -0,0 +1,6 @@
+'''
+Write and test a script that accepts user input using input() , then displays the
+result of trying to find() a particular letter in that input
+'''
+my_string = input("Input some text please ")
+print("I found the letter e at position: ",my_string.find("e"))
